903030845738abe3d541dc4f886e9c02c7c2ccf8
naglit <docono.ne.ip@gmail.com>
2021-02-20 22:53:47 +0900

    added aes code from msdoc

--- Lab.Utility/Encryption/Encryption.cs
+++ Lab.Utility/Encryption/Encryption.cs
@@ -1,40 +1,114 @@
 ﻿using System;
+using System.Collections;
+using System.IO;
 using System.Security.Cryptography;
-using System.Text;
 
 namespace Lab.Utility.Encryption
 {
 	public class Encryption
 	{
-		public byte[] Testing(string input)
-		{
-			var inputs = UTF8Encoding.UTF8.GetBytes(input);
-			return inputs;
-		}
+        
+        public static void EncryptInputParams(Hashtable input)
+        {
+            var original = "Here is some data to encrypt!";
 
-		public static string Encrypt(string input, string key)
-		{
-			var inputs = UTF8Encoding.UTF8.GetBytes(input);
-			var tripleDES = new TripleDESCryptoServiceProvider();
-			tripleDES.Key = UTF8Encoding.UTF8.GetBytes(key);
-			tripleDES.Mode = CipherMode.ECB;
-			tripleDES.Padding = PaddingMode.PKCS7;
-			ICryptoTransform cTransform = tripleDES.CreateEncryptor();
-			byte[] resultArray = cTransform.TransformFinalBlock(inputs, 0, inputs.Length);
-			tripleDES.Clear();
-			return Convert.ToBase64String(resultArray, 0, resultArray.Length);
-		}
-		public static string Decrypt(string input, string key)
-		{
-			byte[] inputArray = Convert.FromBase64String(input);
-			TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider();
-			tripleDES.Key = UTF8Encoding.UTF8.GetBytes(key);
-			tripleDES.Mode = CipherMode.ECB;
-			tripleDES.Padding = PaddingMode.PKCS7;
-			ICryptoTransform cTransform = tripleDES.CreateDecryptor();
-			byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
-			tripleDES.Clear();
-			return UTF8Encoding.UTF8.GetString(resultArray);
-		}
-	}
+            // Create a new instance of the AesCryptoServiceProvider
+            // class.  This generates a new key and initialization
+            // vector (IV).
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                // Encrypt the string to an array of bytes.
+                var encrypted = EncryptStringToBytes_Aes(original, aes.Key, aes.IV);
+
+                // Decrypt the bytes to a string.
+                var roundtrip = DecryptStringFromBytes_Aes(encrypted, aes.Key, aes.IV);
+
+                //Display the original data and the decrypted data.
+                Console.WriteLine("Original:   {0}", original);
+                Console.WriteLine("Round Trip: {0}", roundtrip);
+            }
+        }
+        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Create an AesCryptoServiceProvider object
+            // with the specified key and IV.
+            using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an AesCryptoServiceProvider object
+            // with the specified key and IV.
+            using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+    }
 }
