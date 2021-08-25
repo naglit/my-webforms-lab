cda0afeb931e31f0dd2c46cdda2d4d4bd979478f
naglit <docono.ne.ip@gmail.com>
2021-08-25 17:44:11 +0900

    try out addrange

--- Lab.Utility/Csharp/Csharp.cs
+++ Lab.Utility/Csharp/Csharp.cs
@@ -224,5 +224,61 @@ namespace Lab.Utility.Csharp
 									)"; 
 			} 
 		}
+		public static void TryAddRange()
+		{
+			string[] input = { "Brachiosaurus",
+						   "Amargasaurus",
+						   "Mamenchisaurus" };
+
+			List<string> dinosaurs = new List<string>(input);
+
+			Console.WriteLine("\nCapacity: {0}", dinosaurs.Capacity);
+
+			Console.WriteLine();
+			foreach (string dinosaur in dinosaurs)
+			{
+				Console.WriteLine(dinosaur);
+			}
+
+			Console.WriteLine("\nAddRange(dinosaurs)");
+			dinosaurs.AddRange(dinosaurs);
+
+			Console.WriteLine();
+			foreach (string dinosaur in dinosaurs)
+			{
+				Console.WriteLine(dinosaur);
+			}
+
+			Console.WriteLine("\nRemoveRange(2, 2)");
+			dinosaurs.RemoveRange(2, 2);
+
+			Console.WriteLine();
+			foreach (string dinosaur in dinosaurs)
+			{
+				Console.WriteLine(dinosaur);
+			}
+
+			input = new string[] { "Tyrannosaurus",
+							   "Deinonychus",
+							   "Velociraptor"};
+
+			Console.WriteLine("\nInsertRange(3, input)");
+			dinosaurs.InsertRange(3, input);
+
+			Console.WriteLine();
+			foreach (string dinosaur in dinosaurs)
+			{
+				Console.WriteLine(dinosaur);
+			}
+
+			Console.WriteLine("\noutput = dinosaurs.GetRange(2, 3).ToArray()");
+			string[] output = dinosaurs.GetRange(2, 3).ToArray();
+
+			Console.WriteLine();
+			foreach (string dinosaur in output)
+			{
+				Console.WriteLine(dinosaur);
+			}
+		}
 	}
 }
