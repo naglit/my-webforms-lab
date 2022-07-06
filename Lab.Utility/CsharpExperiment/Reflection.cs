8b0aaffbea05c9403398e2ba8ff6c775fa8cd51d
naglit <docono.ne.ip@gmail.com>
2022-07-06 15:13:03 +0900

    reflection

---
+++ Lab.Utility/CsharpExperiment/Reflection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Utility.CsharpExperiment
+{
+	public class Reflection
+	{
+		public static void CompareWithGenericType<T>()
+		{
+			var sampleA = new SampleClassA();
+			var sampleB = new SampleClassA();
+			var answer = sampleA.GetType() == typeof(T);
+			Console.WriteLine(answer);
+		}
+	}
+
+	public class SampleClassA : ISample
+	{
+		
+	}
+	public class SampleClassB : ISample
+	{
+
+	}
+
+	public interface ISample
+	{
+		
+	}
+}
--- Lab_ConsoleApp/Program.cs
+++ Lab_ConsoleApp/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Lab.Utility.Csharp;
 
 namespace Lab_ConsoleApp
 {
