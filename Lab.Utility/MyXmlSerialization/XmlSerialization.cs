1f415666e4073ade960b12bbd39c441f7ba2f688
naglit <docono.ne.ip@gmail.com>
2023-03-08 17:48:00 +0900

    xmlserialization

---
+++ Lab.Utility/MyXmlSerialization/XmlSerialization.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Lab.Utility.MyXmlSerialization
+{
+	public class XmlSerialization
+	{
+		public static void Main()
+		{
+			using (var stringwriter = new StringWriter())
+			{
+				var xns = new XmlSerializerNamespaces();
+				xns.Add("", "");
+				var req = new Req();
+				var serializer = new XmlSerializer(req.GetType());
+				serializer.Serialize(stringwriter, new Req(), xns);
+				Console.WriteLine(stringwriter.ToString());
+			}
+		}
+	}
+
+	public class Req
+	{
+		public Target Target { get; set; } = new Target();
+	}
+
+	public class Target
+	{
+		public Target()
+		{
+			this.Field = string.Join(",", this.Feilds);
+		}
+		public string OrderId { get; set; } = "00000001";
+		public string Field { get; set; }
+		[XmlIgnore]
+		public string[] Feilds { get; set; } = { "name", "id", "gender" };
+	}
+}
