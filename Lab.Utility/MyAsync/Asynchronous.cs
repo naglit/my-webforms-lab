8417a4982ef731c6dc9ee75598ea9353ea37b51c
naglit <docono.ne.ip@gmail.com>
2023-03-08 13:52:49 +0900

    asynchronous

---
+++ Lab.Utility/MyAsync/Asynchronous.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Utility.MyAsync
+{
+	public class Asynchronous
+	{
+		public static void Main()
+		{
+			SomeMethod();
+			Console.WriteLine("hey");
+		}
+
+		public static async void SomeMethod()
+		{
+			Console.WriteLine("start");
+			await Task.Delay(5000);
+			Console.WriteLine("end");
+		}
+	}
+}
